af6ed3dcb28b03848c844e1f6a1ecb2660dc1036
caojunatthu <caojunatthu@163.com>
2017-05-27 13:56:05 +0800

    add a usable end algorithom

--- Dispatcher.go
+++ Dispatcher.go
@@ -14,6 +14,15 @@ type ResponsePack struct {
 	response *Response
 	callback RequestCallback
 }
+type RoutineStatus struct {
+	running bool
+	//registerTime time.Time
+	routineInd int
+}
+
+func (self *RoutineStatus)String()string{
+	return fmt.Sprintf("running:%v, routineInd:%v", self.running, self.routineInd)
+}
 
 type ItemMidwareFunc func (IBaseItem) bool
 type RequestMidwareFunc func (*Request) bool
@@ -32,7 +41,7 @@ type Dispatcher struct {
 	requestCacheChan chan *Request
 	requestChan chan *Request
 	responseChan chan *ResponsePack
-	heartChan chan int
+	heartChan chan *RoutineStatus
 }
 
 func NewDispatcher() *Dispatcher{
@@ -49,7 +58,7 @@ func NewDispatcher() *Dispatcher{
 		requestCacheChan:make(chan *Request),//这个只要一路就好了,总管分发
 		requestChan:make(chan *Request, MAX_PROCS),
 		responseChan:make(chan *ResponsePack, MAX_PROCS),
-		heartChan:make(chan int, MAX_PROCS),
+		heartChan:make(chan *RoutineStatus, MAX_PROCS),
 	}
 	return self
 }
@@ -63,38 +72,79 @@ func (self *Dispatcher)SetNetService(net *NetService){
 }
 
 func (self *Dispatcher)Dispatch(spiders... IBaseSpider){
-	go self.dispatchRequests(spiders...)
+	totalRoutineCount := 0
+	go self.dispatchRequests(totalRoutineCount, spiders...)
+	totalRoutineCount += 1
 
-	for i := 0; i < Max(cap(self.requestChan), 1); i++ {
-		go self.handleRequest(i)
+	routineCount := Max(cap(self.requestChan), 1)
+	for i := 0; i < routineCount; i++ {
+		go self.handleRequest(totalRoutineCount + i)
 	}
-	for i := 0; i < Max(cap(self.itemChan), 1); i++ {
-		go self.handleItem(i)
+	totalRoutineCount += routineCount
+	routineCount = Max(cap(self.itemChan), 1)
+	for i := 0; i < routineCount; i++ {
+		go self.handleItem(totalRoutineCount + i)
 	}
-	for i := 0; i < Max(cap(self.responseChan), 1); i++ {
-		go self.handleResponse(i)
+	totalRoutineCount += routineCount
+	routineCount = Max(cap(self.itemChan), 1)
+	for i := 0; i < routineCount; i++ {
+		go self.handleResponse(totalRoutineCount + i)
 	}
-	self.WaitAllDone()
+	totalRoutineCount += routineCount
+	self.waitAllDone(totalRoutineCount)
 	fmt.Printf("All Done!!\n")
 }
 
-func (self *Dispatcher)WaitAllDone(){
-	const DURATION = 10 * time.Second
+func (self *Dispatcher)waitAllDone(totalRoutineCount int){
+	const DURATION = 2 * time.Second
 	timer := time.NewTimer(DURATION)
+
+	var subRoutinesStatus [2][]*RoutineStatus
+	for ind := range subRoutinesStatus{
+		subRoutinesStatus[ind] = make([]*RoutineStatus, totalRoutineCount)
+	}
+	curInd := 0
 
 	OUTER_LOOP:
 	for {
 		timer.Reset(DURATION)
 		select {
 		case v := <-self.heartChan:
+			subRoutinesStatus[curInd][v.routineInd] = v
 			fmt.Printf("heart beating....%v\n", v)
 		case <-timer.C:
-			break OUTER_LOOP
+			fmt.Printf("time out \n")
+			isAllNil := true
+			for _, status := range subRoutinesStatus[curInd]{
+				if status != nil && status.running{//有活着的超时了
+					fmt.Printf("has some routine working\n")
+					continue OUTER_LOOP
+				} else if status != nil{
+					isAllNil = false
+				}
+			}
+			if isAllNil{
+				fmt.Printf("all nil, get out\n")
+				break OUTER_LOOP//真正全部结束了
+			}
+
+			fmt.Printf("give another chance to wait\n")
+			curInd = (curInd + 1) % 2
+			//等下一次超时过来，看看你们是不是真的全死了
+			for ind := range subRoutinesStatus[curInd]{
+				subRoutinesStatus[curInd][ind] = nil
+			}
 		}
 	}
 }
 
-func (self *Dispatcher)dispatchRequests(spiders ...IBaseSpider){
+func (self *Dispatcher)registerStatus(ind int, running bool){
+	//self.heartChan<- &RoutineStatus{running:running, registerTime:time.Now(), routineInd:ind}
+	self.heartChan<- &RoutineStatus{running:running, routineInd:ind}
+}
+
+func (self *Dispatcher)dispatchRequests(ind int, spiders ...IBaseSpider){
+	self.registerStatus(ind, true)
 	const MAX_BUFFER_COUNT = 200
 	var requests []*Request = make([]*Request, 0, MAX_BUFFER_COUNT)
 	handleMidware := func(request *Request){
@@ -105,6 +155,7 @@ func (self *Dispatcher)dispatchRequests(spiders ...IBaseSpider){
 		}
 	}
 	insertRequest := func (request *Request){
+		self.registerStatus(ind, true)
 		handleMidware(request)
 		if request == nil{
 			return
@@ -130,6 +181,7 @@ func (self *Dispatcher)dispatchRequests(spiders ...IBaseSpider){
 
 	//handledRequestCount := 0
 	for {
+		self.registerStatus(ind, false)
 		if len(requests) != 0 {
 			select {
 			case request := <-self.requestCacheChan://优先这个,好大概保持个顺序
@@ -140,6 +192,7 @@ func (self *Dispatcher)dispatchRequests(spiders ...IBaseSpider){
 						insertRequest(request)
 					case self.requestChan <- requests[0]:
 					//未测试
+						self.registerStatus(ind, true)
 						requests = requests[1:]
 					//这个操作导致数组越来越大,要适当时候处理下,怎么处理呢,背后的数组都找不到头了,只有拷贝一下
 					//上面的理解可能有误,不会拷贝数组的无用部分的.具体还是得看看源代码了
@@ -162,8 +215,10 @@ func (self *Dispatcher)dispatchRequests(spiders ...IBaseSpider){
 }
 
 func (self *Dispatcher)handleRequest(ind int){
-	sendRequest := func (request *Request){
-		self.heartChan<- 1
+	for {
+		self.registerStatus(ind, false)
+		request := <-self.requestChan
+		self.registerStatus(ind, true)
 		fmt.Printf("handleRequest send request %v with go %v\n", request, ind)
 		response := self.net.SendRequest(request)
 		for _, midware := range self.responseMidware{
@@ -176,16 +231,13 @@ func (self *Dispatcher)handleRequest(ind int){
 		}
 		self.responseChan<- &ResponsePack{response, request.callback}
 	}
-	for {
-		request := <-self.requestChan
-		sendRequest(request)
-	}
 }
 
 func (self *Dispatcher)handleResponse(ind int){
 	for {
+		self.registerStatus(ind, false)
 		responsePack := <-self.responseChan
-		self.heartChan<- 2
+		self.registerStatus(ind, true)
 		fmt.Printf("handleResponse receive response %v with go %v\n", responsePack.response, ind)
 		requests, items := responsePack.callback(responsePack.response)
 		for _, request := range requests {
@@ -199,8 +251,9 @@ func (self *Dispatcher)handleResponse(ind int){
 
 func (self *Dispatcher)handleItem(ind int){
 	for {
+		self.registerStatus(ind, false)
 		item := <-self.itemChan
-		self.heartChan<- 3
+		self.registerStatus(ind, true)
 		for _, midware := range self.itemMidware{
 			if ! midware(item){//提供终止服务
 				break
